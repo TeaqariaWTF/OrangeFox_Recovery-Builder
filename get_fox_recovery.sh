15449365d09fb6e75566ea9aa9e8751ad2ba86c0
Díwas <109030578+Diwas007@users.noreply.github.com>
2022-11-04 16:57:34 +0545

    Script: add general stuff & cleanup

--- get_fox_recovery.sh
+++ get_fox_recovery.sh
@@ -18,10 +18,10 @@ FOX_BRANCH="fox_12.1"; # default is fox_12.1 (fox_9.0, fox_10.0, fox_11.0, fox_1
 TWRP_BRANCH="twrp-12.1"; # default is twrp-12.1 (twrp-9.0, twrp-10.0, twrp-11.0, twrp-12.1 ?)
 TWRP_MIN_MANIFEST="aosp"; # default is aosp (aosp, lineageos, omni ?)
 DEVICE_BRANCH="fox_12.1"; # device tree branch, default is fox_12.1
-OEM="xiaomi" # default is xiaomi (xiaomi,samsung,etc ?)
-DEVICE_TREE_URL="https://gitlab.com/OrangeFox/device/miatoll.git" # device tree url, default is for miatoll
-LOCAL_DEVICE_TREE_URL="git@gitlab.com:OrangeFox/device/miatoll.git" # local device tree url, default is for miatoll
-FOX_VENDOR_BRANCH="fox_12.1" # default is fox_12.1 (master, fox_10.0, fox_11.0, fox_12.1 ?)
+OEM="xiaomi"; # default is xiaomi (xiaomi,samsung,etc ?)
+DEVICE_TREE_URL="https://gitlab.com/OrangeFox/device/miatoll.git"; # device tree url, default is for miatoll
+LOCAL_DEVICE_TREE_URL="git@gitlab.com:OrangeFox/device/miatoll.git"; # local device tree url, default is for miatoll
+FOX_VENDOR_BRANCH="fox_12.1"; # default is fox_12.1 (master, fox_10.0, fox_11.0, fox_12.1 ?)
 
 # the device whose tree we can clone for compiling a test build
 test_build_device="miatoll"; # default is miatoll
@@ -49,10 +49,10 @@ SYNC_LOG="$BASE_DIR"/"$FOX_BRANCH"_"manifest.sav";
 
 # help
 if [ "$1" = "-h" -o "$1" = "--help" -o "$1" = "help" ]; then
-  echo "Script to set up things for building OrangeFox with the $DEVICE_BRANCH build system"
-  echo "Usage   = $0 [new_manifest_directory]"
-  echo "The default new manifest directory is \"$MANIFEST_DIR\""
-  exit 0
+  echo "Script to set up things for building OrangeFox with the $DEVICE_BRANCH build system";
+  echo "Usage   = $0 [new_manifest_directory]";
+  echo "The default new manifest directory is \"$MANIFEST_DIR\"";
+  exit 0;
 fi
 
 # You can supply a path for the new manifest to override the default
@@ -72,8 +72,8 @@ MANIFEST_BUILD_DIR="$MANIFEST_DIR/build";
   
 # print message and quit
 abort() {
-  echo "$@"
-  exit
+  echo "$@";
+  exit;
 }
 
 # ***************************************************************************************
@@ -83,12 +83,12 @@ abort() {
 # init the script, ensure we have the patch file, and create the manifest directory
 init_script() {
   echo "-- Starting the script ..."
-  [ ! -f "$PATCH_FILE" ] && abort "-- I cannot find the patch file: $PATCH_FILE - quitting!"
+  [ ! -f "$PATCH_FILE" ] && abort "-- I cannot find the patch file: $PATCH_FILE - quitting!";
 
-  echo "-- The new build system will be located in \"$MANIFEST_DIR\""
-  mkdir -p $MANIFEST_DIR
+  echo "-- The new build system will be located in \"$MANIFEST_DIR\"";
+  mkdir -p $MANIFEST_DIR;
   [ "$?" != "0" -a ! -d $MANIFEST_DIR ] && {
-    abort "-- Invalid directory: \"$MANIFEST_DIR\". Quitting."
+    abort "-- Invalid directory: \"$MANIFEST_DIR\". Quitting.";
   }
 }
 
@@ -96,126 +96,132 @@ init_script() {
 get_twrp_minimal_manifest() {
   
   if [ "$TWRP_MIN_MANIFEST" = "aosp" ]; then
-     local MIN_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git"
+     local MIN_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp.git";
   fi
   
   if [ "$TWRP_MIN_MANIFEST" = "omni" ]; then
-     local MIN_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git"
+     local MIN_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git";
   fi
   
-  cd $MANIFEST_DIR
-  echo "-- Initialising the $TWRP_BRANCH minimal manifest repo ..."
-  repo init --depth=1 -u $MIN_MANIFEST -b $TWRP_BRANCH
+  cd $MANIFEST_DIR;
+  echo "-- Initialising the $TWRP_BRANCH minimal manifest repo ...";
+  repo init --depth=1 -u $MIN_MANIFEST -b $TWRP_BRANCH;
   [ "$?" != "0" ] && {
-   abort "-- Failed to initialise the minimal manifest repo. Quitting."
+   abort "-- Failed to initialise the minimal manifest repo. Quitting.";
   }
-  echo "-- Done."
+  echo "-- Done.";
 
-  echo "-- Syncing the $TWRP_BRANCH minimal manifest repo ..."
-  repo sync
+  echo "-- Syncing the $TWRP_BRANCH minimal manifest repo ...";
+  repo sync;
   [ "$?" != "0" ] && {
-   abort "-- Failed to Sync the minimal manifest repo. Quitting."
+   abort "-- Failed to Sync the minimal manifest repo. Quitting.";
   }
-  echo "-- Done."
+  echo "-- Done.";
 }
 
 # patch the build system for OrangeFox
 patch_minimal_manifest() {
-   echo "-- Patching the $TWRP_BRANCH minimal manifest for building OrangeFox for dynamic partition devices ..."
-   cd $MANIFEST_BUILD_DIR
-   patch -p1 < $PATCH_FILE
-   [ "$?" = "0" ] && echo "-- The $TWRP_BRANCH minimal manifest has been patched successfully" || abort "-- Failed to patch the $TWRP_BRANCH minimal manifest! Quitting."
-
+   echo "-- Patching the $TWRP_BRANCH minimal manifest for building OrangeFox for dynamic partition devices ...";
+   cd $MANIFEST_BUILD_DIR;
+   patch -p1 < $PATCH_FILE;
+   [ "$?" = "0" ] && echo "-- The $TWRP_BRANCH minimal manifest has been patched successfully" || abort "-- Failed to patch the $TWRP_BRANCH minimal manifest! Quitting.";
+   
    # save location of manifest dir
-   echo "#" &> $SYNC_LOG
-   echo "MANIFEST_DIR=$MANIFEST_DIR" >> $SYNC_LOG
-   echo "#" >> $SYNC_LOG
+   echo "#" &> $SYNC_LOG;
+   echo "MANIFEST_DIR=$MANIFEST_DIR" >> $SYNC_LOG;
+   echo "#" >> $SYNC_LOG;
 }
 
 # get the qcom/twrp common stuff
 clone_common() {
-local URL
-   cd $MANIFEST_DIR/
+   cd $MANIFEST_DIR/;
 
    if [ ! -d "device/qcom/common" ]; then
-   	echo "-- Cloning qcom common ..."
-	git clone https://github.com/TeamWin/android_device_qcom_common -b $DEVICE_BRANCH device/qcom/common
+   	echo "-- Cloning qcom common ...";
+	git clone https://github.com/TeamWin/android_device_qcom_common -b $DEVICE_BRANCH device/qcom/common;
    fi
 
    if [ ! -d "device/qcom/twrp-common" ]; then
-   	echo "-- Cloning twrp-common ..."
-	git clone https://github.com/TeamWin/android_device_qcom_twrp-common -b $DEVICE_BRANCH device/qcom/twrp-common
+   	echo "-- Cloning twrp-common ...";
+	git clone https://github.com/TeamWin/android_device_qcom_twrp-common -b $DEVICE_BRANCH device/qcom/twrp-common;
    fi
 }
 
 # get the OrangeFox recovery sources
 clone_fox_recovery() {
-local URL=""
+local URL="";
    if [ "$USE_SSH" = "0" ]; then
-      URL="https://gitlab.com/OrangeFox/bootable/Recovery.git"
+      URL="https://gitlab.com/OrangeFox/bootable/Recovery.git";
    else
-      URL="git@gitlab.com:OrangeFox/bootable/Recovery.git"
+      URL="git@gitlab.com:OrangeFox/bootable/Recovery.git";
    fi
 
    mkdir -p $MANIFEST_DIR/bootable
    [ ! -d $MANIFEST_DIR/bootable ] && {
-      echo "-- Invalid directory: $MANIFEST_DIR/bootable"
-      return
+      echo "-- Invalid directory: $MANIFEST_DIR/bootable";
+      return;
    }
 
-   cd $MANIFEST_DIR/bootable/
+   cd $MANIFEST_DIR/bootable/;
    [ -d recovery/ ] && {
-      echo  "-- Moving the TWRP recovery sources to /tmp"
-      rm -rf /tmp/recovery
-      mv recovery /tmp
+      echo  "-- Moving the TWRP recovery sources to /tmp";
+      rm -rf /tmp/recovery;
+      mv recovery /tmp;
    }
 
-   echo "-- Pulling the OrangeFox recovery sources ..."
-   git clone --recurse-submodules $URL -b $FOX_BRANCH recovery
-   [ "$?" = "0" ] && echo "-- The OrangeFox sources have been cloned successfully" || echo "-- Failed to clone the OrangeFox sources!"
+   echo "-- Pulling the OrangeFox recovery sources ...";
+   git clone --recurse-submodules $URL -b $FOX_BRANCH recovery;
+   [ "$?" = "0" ] && echo "-- The OrangeFox sources have been cloned successfully" || echo "-- Failed to clone the OrangeFox sources!";
 
    # check that the themes are correctly downloaded
    if [ ! -f recovery/gui/theme/portrait_hdpi/ui.xml ]; then
-      	echo "-- Themes not found! Trying again to pull the themes ..."
+      	echo "-- Themes not found! Trying again to pull the themes ...";
    	if [ "$USE_SSH" = "0" ]; then
-      	   URL="https://gitlab.com/OrangeFox/misc/theme.git"
+      	   URL="https://gitlab.com/OrangeFox/misc/theme.git";
    	else
-      	   URL="git@gitlab.com:OrangeFox/misc/theme.git"
+      	   URL="git@gitlab.com:OrangeFox/misc/theme.git";
    	fi
-      	[ -d recovery/gui/theme ] && rm -rf recovery/gui/theme
-      	git clone $URL recovery/gui/theme
-      	[ "$?" = "0" ] && echo "-- The themes have been cloned successfully" || echo "-- Failed to clone the themes!"
+      	[ -d recovery/gui/theme ] && rm -rf recovery/gui/theme;
+      	git clone $URL recovery/gui/theme;
+      	[ "$?" = "0" ] && echo "-- The themes have been cloned successfully" || echo "-- Failed to clone the themes!";
    fi
    
+   # ensure that the submodules are updated
+   if [ -d $MANIFEST_DIR/bootable/recovery/gui/theme ]; then
+      cd $MANIFEST_DIR/bootable/recovery/;
+      git submodule foreach --recursive git pull origin master;
+      cd $MANIFEST_DIR/bootable/;
+   fi
+
    # cleanup /tmp/recovery/
-   echo  "-- Cleaning up the TWRP recovery sources from /tmp"
-   rm -rf /tmp/recovery
+   echo  "-- Cleaning up the TWRP recovery sources from /tmp";
+   rm -rf /tmp/recovery;
    
    # create the directory for Xiaomi device trees
-   mkdir -p $MANIFEST_DIR/device/xiaomi
+   mkdir -p $MANIFEST_DIR/device/xiaomi;
 }
 
 # get the OrangeFox vendor
 clone_fox_vendor() {
-local URL
+local URL="";
    if [ "$USE_SSH" = "0" ]; then
-      URL="https://gitlab.com/OrangeFox/vendor/recovery.git"
+      URL="https://gitlab.com/OrangeFox/vendor/recovery.git";
    else
-      URL="git@gitlab.com:OrangeFox/vendor/recovery.git"
+      URL="git@gitlab.com:OrangeFox/vendor/recovery.git";
    fi
    
-   echo "-- Preparing for cloning the OrangeFox vendor tree ..."
-   rm -rf $MANIFEST_DIR/vendor/recovery
-   mkdir -p $MANIFEST_DIR/vendor
+   echo "-- Preparing for cloning the OrangeFox vendor tree ...";
+   rm -rf $MANIFEST_DIR/vendor/recovery;
+   mkdir -p $MANIFEST_DIR/vendor;
    [ ! -d $MANIFEST_DIR/vendor ] && {
-      echo "-- Invalid directory: $MANIFEST_DIR/vendor"
-      return
+      echo "-- Invalid directory: $MANIFEST_DIR/vendor";
+      return;
    }
    
-   cd $MANIFEST_DIR/vendor
-   echo "-- Pulling the OrangeFox vendor tree ..."
-   git clone $URL -b $FOX_VENDOR_BRANCH recovery
-   [ "$?" = "0" ] && echo "-- The OrangeFox vendor tree has been cloned successfully" || echo "-- Failed to clone the OrangeFox vendor tree!"
+   cd $MANIFEST_DIR/vendor;
+   echo "-- Pulling the OrangeFox vendor tree ...";
+   git clone $URL -b $FOX_VENDOR_BRANCH recovery;
+   [ "$?" = "0" ] && echo "-- The OrangeFox vendor tree has been cloned successfully" || echo "-- Failed to clone the OrangeFox vendor tree!";
 }
 
 
@@ -240,80 +246,80 @@ local BRANCH="android-9.0";
 
 # get device trees
 get_device_tree() {
-local DIR=$MANIFEST_DIR/device/$OEM
-   mkdir -p $DIR
-   cd $DIR
+local DIR=$MANIFEST_DIR/device/xiaomi;
+   mkdir -p $DIR;
+   cd $DIR;
    [ "$?" != "0" ] && {
-      abort "-- get_device_tree() - Invalid directory: $DIR"
+      abort "-- get_device_tree() - Invalid directory: $DIR";
    }
 
    # test device
-   local URL=$LOCAL_DEVICE_TREE_URL
-   [ "$USE_SSH" = "0" ] && URL=$DEVICE_TREE_URL
-   echo "-- Pulling the $test_build_device device tree ..."
-   git clone $URL -b $DEVICE_BRANCH "$test_build_device"
+   local URL=$LOCAL_DEVICE_TREE_URL;
+   [ "$USE_SSH" = "0" ] && URL=$DEVICE_TREE_URL;
+   echo "-- Pulling the $test_build_device device tree ...";
+   git clone $URL -b $DEVICE_BRANCH "$test_build_device";
 
    # done
    if [ -d "$test_build_device" -a -d "$test_build_device/recovery" ]; then
-      echo "-- Finished fetching the OrangeFox $test_build_device device tree."
+      echo "-- Finished fetching the OrangeFox $test_build_device device tree.";
    else
-      abort "-- get_device_tree() - could not fetch the OrangeFox $test_build_device device tree."
+      abort "-- get_device_tree() - could not fetch the OrangeFox $test_build_device device tree.";
    fi
 }
 
 # test build
 test_build() {
    # clone the device tree
-   get_device_tree
+   get_device_tree;
 
    # proceed with the test build
    if [ "$FOX_VERSION" = "1" ]; then
-      export FOX_VERSION="$FOX_BASE_VERSION"_"$FOX_BRANCH"
+      export FOX_VERSION="$FOX_BASE_VERSION"_"$FOX_BRANCH";
    fi
    
    if [ "$FOX_BUILD_TYPE" = "1" ]; then
-      export FOX_BUILD_TYPE="Alpha"
+      export FOX_BUILD_TYPE="Alpha";
    fi
    
    # common
-   export ALLOW_MISSING_DEPENDENCIES=true
-   export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-   export LC_ALL="C"
+   export ALLOW_MISSING_DEPENDENCIES=true;
+   export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1;
+   export LC_ALL="C";
    
-   export FOX_BUILD_DEVICE="$test_build_device"
-   export OUT_DIR=$BASE_DIR/BUILDS/"$test_build_device"
+   export FOX_BUILD_DEVICE="$test_build_device";
+   export OUT_DIR=$BASE_DIR/BUILDS/"$test_build_device";
    
-   cd $BASE_DIR/
-   mkdir -p $OUT_DIR
+   cd $BASE_DIR/;
+   mkdir -p $OUT_DIR;
    
-   cd $MANIFEST_DIR/
-   echo "-- Compiling a test build for device \"$test_build_device\". This will take a *VERY* long time ..."
-   echo "-- Start compiling: "
-   . build/envsetup.sh
+   cd $MANIFEST_DIR/;
+   echo "-- Compiling a test build for device \"$test_build_device\". This will take a *VERY* long time ...";
+   echo "-- Start compiling: ";
+   . build/envsetup.sh;
 
    # build for the device
    if [ "$FOX_OMNI_VAB_DEVICE" = "1" ]; then
-      lunch omni_"$test_build_device"-eng && mka bootimage
+      lunch omni_"$test_build_device"-eng && mka bootimage;
    else
-      lunch omni_"$test_build_device"-eng && mka recoveryimage
+      lunch omni_"$test_build_device"-eng && mka recoveryimage;
    fi
 
    if [ "$FOX_AOSP_VAB_DEVICE" = "1" ]; then
-      lunch twrp_"$test_build_device"-eng && mka adbd bootimage
+      lunch twrp_"$test_build_device"-eng && mka adbd bootimage;
    else
-      lunch twrp_"$test_build_device"-eng && mka adbd recoveryimage
+      lunch twrp_"$test_build_device"-eng && mka adbd recoveryimage;
    fi
    
    # any results?
-   ls -all $(find "$OUT_DIR" -name "OrangeFox-*")
+   ls -all $(find "$OUT_DIR" -name "OrangeFox-*");
 }
 
 # upload the build
 upload_build() {
      # Download transfer
-     curl -sL https://git.io/file-transfer | sh
+     curl -sL https://git.io/file-transfer | sh;
      # Let's upload the build
-     ./transfer wet $OUT_DIR/target/product/"$test_build_device"/OrangeFox-*.zip
+     ./transfer wet $OUT_DIR/target/product/"$test_build_device"/OrangeFox-*.zip;
 }
 
 # do all the work!
